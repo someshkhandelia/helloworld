f216fd1e8a3612295dda974c995d90146bf62035
Somesh Khandelia <someshkhandelia@gmail.com>
2014-10-18 19:08:11 +0530

    second python program

---
+++ second.py
@@ -0,0 +1,12 @@
+"""this program wil print the members of the nested list and indent them"""
+movies=["dark knight","troy",["spiderman","ps i love you",["rush hour","enter the dragon"]]]
+def print_lol(a_list,level):
+    for each_item in a_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tab in range(level):
+                print("\t"),
+
+            print(each_item)    
+print_lol(movies,0)
